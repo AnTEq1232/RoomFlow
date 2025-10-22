a2c3bf8f722ef37114ef47a286ca17a44939d457
Antek <anteksalamon6@gmail.com>
2025-10-22 22:09:33 +0200

    to do

--- api/getReservation.php
+++ api/getReservation.php
@@ -1,13 +1,25 @@
 <?php
     include 'db/db.php';
-    
     header('Content-Type: application/json');
-    
-    $example = [
-        "name" => "Antek",
-        "age" => 18
-    ];
+    error_reporting(0); // wyłącza błędy w HTML
 
-    echo json_encode($example);
+    $room = $_GET['room'];
+    $date = $_GET['date'];
 
+    if (!$room || !$date) {
+        echo json_encode(["error" => "Brak parametrów"]);
+        exit;
+    }
+
+    $sql = "SELECT start_time, end_time FROM reservations WHERE room_id = $room AND date = '$date'";
+    $result = $conn->query($sql);
+
+    $reservations = [
+        'start_time' => $result->fetch_assoc()['start_time'],
+        'end_time' => $result->fetch_assoc()['end_time']
+    ]
+
+    echo json_encode($reservations);
+
+    $conn->close();
 ?>
--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-// document.getElementById("reserveForm").addEventListener("submit", function(event) {
-//     event.preventDefault();
-    
-//     const room = document.getElementById("roomNumber").value;
-//     const date = document.getElementById("data").value;
-//     const startTime = document.getElementById("startTime").value;
-//     const endTime = document.getElementById("endTime").value;
-//     const error = document.getElementById("error");
-
-//     if(!room || !date || !startTime || !endTime){ 
-//         error.style.display = "block";
-//         error.textContent = "Please fill in all fields";
-//         return;
-//     }
-
-    
-// });
+// fetch("api/getReservation.php")
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log("Otrzymane dane z PHP:", data);
+//         alert("Sprawdź konsolę (F12) — dane zostały pobrane");
+//     })
+//     .catch(err => console.error("Błąd fetch:", err));
 
 
-fetch("api/getReservation.php")
-    .then(res => res.json())
+document.getElementById("reserveForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const room = document.getElementById("roomNumber").value;
+    const date = document.getElementById("data").value;
+
+fetch(`api/getReservation.php?room=${room}&date=${date}`)
+    .then(response => response.json())
     .then(data => {
-        console.log(data);
+        console.log("Otrzymane dane z PHP:", data);
+        alert("Sprawdź konsolę (F12) — dane zostały pobrane");
     })
-    .catch(err => console.log(err));
+    .catch(err => console.error("Błąd fetch:", err));
+
+
+});
