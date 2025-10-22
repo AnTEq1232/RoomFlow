1af403b325faaccf7fce15d6de5ec7b1a57923bd
Antek <anteksalamon6@gmail.com>
2025-10-22 16:49:34 +0200

    style changes

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<header>
+        <a href="index.php"><img src="image/logo.png" alt="logo"></a>
+</header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="reserve.php">Reserve</a></li>
+            <li><a href="rooms.php">Rooms</a></li>
+            <li><a href="equipment.php">Equipment</a></li>
+        </ul>
+        <section class="userSection">
+            <h3 class="userName" style="display: none;">Nie zalogowano</h3>
+            <p><a href="login.php">Zaloguj</a></p>
+        </section>
+    </nav>
+    <hr>
--- index.php
+++ index.php
@@ -3,25 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Room Flow</title>
 </head>
 <body>
-    <header>
-        <img src="image/logo.png" alt="logo">
-    </header>
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="reserve.php">Reserve</a></li>
-            <li><a href="reserve.php">Rooms</a></li>
-            <li><a href="reserve.php">Equipment</a></li>
-        </ul>
-        <section class="userSection">
-            <h3 class="userName">Nie zalogowano</h3>
-            <p><a href="login.php">Zaloguj</a></p>
-        </section>
-    </nav>
+    <?php include 'includes/header.php'; ?>
     <main>
         <section>
             smth will be here
