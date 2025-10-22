5938e4e03895f6362e549241b4307ef735ff8996
Antek <anteksalamon6@gmail.com>
2025-10-22 20:49:38 +0200

    will be fixed

--- api/getReservation.php
+++ api/getReservation.php
@@ -1,34 +1,13 @@
-<?php 
+<?php
     include 'db/db.php';
-
+    
     header('Content-Type: application/json');
     
-    $room = $_GET['room'];
-    $date = $_GET['date'];  
+    $example = [
+        "name" => "Antek",
+        "age" => 18
+    ];
 
-    if(!$room || !$date) {
-        echo json_encode(array('error' => 'Missing room or date parameter'));
-        exit;
-    }
-
-    $getReservationQuery = "SELECT start_time, end_time FROM reservations WHERE room = ? AND date = ?";
-    
-    $stmt = $conn->prepare($getReservationQuery);
-    $stmt->bind_param("ss", $room, $date);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    $reservation = [];
-
-    while($row = $result->fetch_assoc()) {
-        $reservation[] = [
-            'start_time' => $row['start_time'],
-            'end_time' => $row['end_time']
-        ]
-    }
-    echo json_encode($reservation);
-
-    $stmt->close();
-    $conn->close();
+    echo json_encode($example);
 
 ?>
--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
-document.getElementById("reserveForm").addEventListener("submit", function(event) {
-    event.preventDefault();
+// document.getElementById("reserveForm").addEventListener("submit", function(event) {
+//     event.preventDefault();
     
-    const room = document.getElementById("roomNumber").value;
-    const data = document.getElementById("data").value;
-    const startTime = document.getElementById("startTime").value;
-    const endTime = document.getElementById("endTime").value;
-    const error = document.getElementById("error");
+//     const room = document.getElementById("roomNumber").value;
+//     const date = document.getElementById("data").value;
+//     const startTime = document.getElementById("startTime").value;
+//     const endTime = document.getElementById("endTime").value;
+//     const error = document.getElementById("error");
 
-    if(!room || !data || !startTime || !endTime){
-        error.style.display = "block";
-        error.textContent = "Please fill in all fields";
-        return;
-    }
+//     if(!room || !date || !startTime || !endTime){ 
+//         error.style.display = "block";
+//         error.textContent = "Please fill in all fields";
+//         return;
+//     }
+
     
-    fetch(`api/getReservation.php?room=${encodeURIComponent(room)}&data=${encodeURIComponent(data)}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            error.textContent = "Cos bylo"; 
-            error.style.display="block";
-        });
-});
+// });
+
+
+fetch("api/getReservation.php")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => console.log(err));
